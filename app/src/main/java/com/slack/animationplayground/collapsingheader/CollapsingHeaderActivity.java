05ebba00d87dafc3d1719daeeced6d5370088b56
Mike Worth <mike@slack-corp.com>
2016-09-03 08:14:50 -0700

    Don't use the default auto-transition for the collapsing header demo

--- app/src/main/java/com/slack/animationplayground/collapsingheader/CollapsingHeaderActivity.java
+++ app/src/main/java/com/slack/animationplayground/collapsingheader/CollapsingHeaderActivity.java
@@ -3,7 +3,11 @@ package com.slack.animationplayground.collapsingheader;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.transition.ChangeBounds;
+import android.support.transition.Fade;
 import android.support.transition.TransitionManager;
+import android.support.transition.TransitionSet;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +22,10 @@ public class CollapsingHeaderActivity extends AppCompatActivity {
 
   @Bind(R.id.root_view)
   ViewGroup rootView;
+  @Bind(R.id.header_container)
+  ViewGroup headerContainer;
+  @Bind(R.id.bottom_view)
+  View bottomView;
   @Bind(R.id.header_title_collapsed)
   View headerTitleCollapsed;
   @Bind(R.id.header_title_expanded)
@@ -34,7 +42,55 @@ public class CollapsingHeaderActivity extends AppCompatActivity {
 
   @OnClick(R.id.header_container)
   void onClickHeader() {
-    TransitionManager.beginDelayedTransition(rootView);
+    /**
+     * By doing a transition set we are able to tweak delays, ordering, interpolators. This gives
+     * a more smooth feel than the default auto-transition which does a fade out, change bounds, and
+     * fade in for the views that changed in sequence.
+     *
+     * Comment out the TransitionSet code and remove that argument from the delayed transition method
+     * to compare the custom to the default transition.
+     *
+     * Note: This could be all done in XML instead to keep the java source tidy.
+     */
+    TransitionSet transitionSet = new TransitionSet();
+
+    if (headerTitleCollapsed.getVisibility() == View.VISIBLE) {
+      transitionSet.addTransition(
+          new Fade(Fade.OUT)
+              .addTarget(headerTitleCollapsed));
+
+      transitionSet.addTransition(
+          new ChangeBounds()
+              .addTarget(headerContainer)
+              .addTarget(bottomView));
+
+      transitionSet.addTransition(
+          new Fade(Fade.IN)
+              .setStartDelay(300)
+              .addTarget(headerTitleExpanded)
+              .addTarget(headerTextExpanded));
+    } else {
+      transitionSet.addTransition(
+          new Fade(Fade.OUT)
+              .addTarget(headerTitleExpanded)
+              .addTarget(headerTextExpanded));
+
+      transitionSet.addTransition(
+          new ChangeBounds()
+              .setStartDelay(150)
+              .addTarget(headerContainer)
+              .addTarget(bottomView));
+
+      transitionSet.addTransition(
+          new Fade(Fade.IN)
+              .setStartDelay(300)
+              .addTarget(headerTitleCollapsed));
+    }
+
+    transitionSet.setOrdering(TransitionSet.ORDERING_TOGETHER)
+        .setInterpolator(new FastOutSlowInInterpolator());
+
+    TransitionManager.beginDelayedTransition(rootView, transitionSet);
     toggleVisibility(headerTitleCollapsed);
     toggleVisibility(headerTitleExpanded);
     toggleVisibility(headerTextExpanded);
